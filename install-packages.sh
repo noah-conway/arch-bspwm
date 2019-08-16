243055bbf0c7aef4cf4f99b0ae436f685623f60a
noah <nconway@posteo.net>
2019-08-15 20:50:00 -0700

    first commit

---
+++ install-packages.sh
@@ -0,0 +1,3 @@
+pacman -S xorg-server xorg-xinit linux-lts networkmanager network-manager-applet p7zip unrar tar dd rsync bspwm sxhkd dmenu noto-fonts noto-fonts-extra noto-fonts-cjk openssh pass oath-toolkit vifm calcurse mpd mpc ncmpcpp beets 
+
+
